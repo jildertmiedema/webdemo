12d401a24685a63cb3bcacc41c40d31f0d311a23
jildertmiedema <git@jildertmiedema.nl>
2018-09-24 21:43:54 +0200

    test route

--- src/main/java/nl/juntos/gyc/webdemo/DemoApplication.java
+++ src/main/java/nl/juntos/gyc/webdemo/DemoApplication.java
@@ -2,11 +2,22 @@ package nl.juntos.gyc.webdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
+
+@RestController
+class TestController {
+	@GetMapping("/")
+	public String test() {
+		return "test";
 	}
 }
